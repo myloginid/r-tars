e323213fa4d28f875f785f36ecb3cdaeee7449a7
Manish Maheshwari <myloginid@gmail.com>
2018-05-11 00:19:40 +0800

    Update download_r_packages.R

--- download_r_packages.R
+++ download_r_packages.R
@@ -1,9 +1,29 @@
 
+# Remove all packages that R does not provide
+# Shamlessly stolen from:
+# https://www.r-bloggers.com/how-to-remove-all-user-installed-packages-in-r/
+
+# create a list of all installed packages
+ ip <- as.data.frame(installed.packages())
+ head(ip)
+# if you use MRO, make sure that no packages in this library will be removed
+ ip <- subset(ip, !grepl("MRO", ip$LibPath))
+# we don't want to remove base or recommended packages either\
+ ip <- ip[!(ip[,"Priority"] %in% c("base", "recommended")),]
+# determine the library where the packages are installed
+ path.lib <- unique(ip$LibPath)
+# create a vector with all the names of the packages you want to remove
+ pkgs.to.remove <- ip[,1]
+ head(pkgs.to.remove)
+# remove the packages
+ sapply(pkgs.to.remove, remove.packages, lib = path.lib)
+
+
 # On Server with Internet
 getPackages <- function(packs){
   packages <- unlist(
     tools::package_dependencies(packs, available.packages(),
-                                which=c("Depends", "Suggests", "Imports"), recursive=TRUE)
+                                which=c("Depends", "Imports"), recursive=TRUE)
   )
   packages <- union(packs, packages)
   packages
