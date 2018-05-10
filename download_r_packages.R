7e3e2b7e9fc7eca1d6d3bef0b625a504221a3169
Manish Maheshwari <myloginid@gmail.com>
2018-05-11 00:09:28 +0800

    Update download_r_packages.R

--- download_r_packages.R
+++ download_r_packages.R
@@ -3,7 +3,7 @@
 getPackages <- function(packs){
   packages <- unlist(
     tools::package_dependencies(packs, available.packages(),
-                                which=c("Depends", "Imports"), recursive=TRUE)
+                                which=c("Depends", "Suggests", "Imports"), recursive=TRUE)
   )
   packages <- union(packs, packages)
   packages
